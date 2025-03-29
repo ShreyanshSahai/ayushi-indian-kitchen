7e513ff6a8d697a957790ba3169ad33e17e0f182
Shreyansh Sahai <sahaishreyansh@gmail.com>
2025-03-29 15:57:19 +0530

    Image redering resolved

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
-const path = require("path");
-
-module.exports = {
-    webpack: (config, { isServer }) => {
-        // Add file-loader for specific file types
-        config.module.rules.push({
-            test: /\.(png|jpg|gif|svg)$/,
-            use: [
-                {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        outputPath: "static/images/",
-                        publicPath: "/_next/static/images/",
-                    },
-                },
-            ],
-        });
-
-        return config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "placehold.co",
+            },
+        ],
     },
 };
+
+module.exports = nextConfig;
